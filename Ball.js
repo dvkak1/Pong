bd6ee246f0cfeec6e6580385deb4c12875635f10
dvkak1 <dv_kakilala@yahoo.com>
2025-07-28 17:28:24 +0800

    javascript code for ball animation updated

--- Ball.js
+++ Ball.js
@@ -56,14 +56,14 @@ export default class Ball {
   update(delta) {
    this.x += this.direction.x * this.velocity * delta
    this.y += this.direction.y * this.velocity * delta
-//    this.velocity += VELOCITY_INCREASE * delta
+   this.velocity += VELOCITY_INCREASE * delta
    const rect = this.rect()
    
    if (rect.bottom >= window.innerHeight || rect.top <= 0) {
      this.direction.y *= -1
    }
    if (rect.right >= window.innerHeight || rect.left <= 0) {
-     this.direction.y *= -1
+     this.direction.x *= -1
    }
 }
 
